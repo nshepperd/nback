98ab0b0b879a11f3ec383f3142ded8b9061b8a53
Neil Shepperd <nshepperd@gmail.com>
2010-12-04 17:17:08 +1100

    Add some basic docs. Make the program create a pipe automatically.

--- nback.py
+++ nback.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python2
 # encoding: UTF-8
-import pygame, os, sys
+import pygame, os, sys, stat
 import pygame.font
 import pygame.gfxdraw
 import time, random
+
+def choice(set):
+    return random.choice(list(set))
 
 def message(screen, text):
     raster = font.render(text, True, (10, 255, 10))
@@ -24,8 +27,8 @@ def drawaxes(screen):
     for y in (100, 200):
         pygame.gfxdraw.hline(screen, 5, 295, y, color)
 
-visual_points = set([(0, 0), (0, 1), (0, 2), (2, 0), (2, 1), (2, 2), (1, 0), (1, 2)])
-audio_points = set("avsomuit")
+visual_points = {(0, 0), (0, 1), (0, 2), (2, 0), (2, 1), (2, 2), (1, 0), (1, 2)}
+audio_points = set("AVSOMUIT")
 
 def gentrials(n, points):
     trials = 20 + n
@@ -39,11 +42,12 @@ def gentrials(n, points):
         if target[i]:
             p = tlist[i]
         else:
-            p = random.choice(list(points.difference(set([tlist[i]]))))
+            p = choice(points.difference({tlist[i]}))
         tlist.append(p)
     return tlist
 
 def oneback(screen, n):
+    """Single n-back"""
     trials = 20 + n
     visual = gentrials(n, visual_points)
     score = 0
@@ -88,6 +92,7 @@ def oneback(screen, n):
     return score
 
 def twoback(screen, n):
+    """Dual n-back"""
     trials = 20 + n
     visual = gentrials(n, visual_points)
     audio = gentrials(n, audio_points)
@@ -99,7 +104,6 @@ def twoback(screen, n):
     time.sleep(2.0)
     pygame.event.clear()
     for i in range(trials):
-        vmark, amark = (False, False)
         screen.fill((0, 0, 0))
         drawaxes(screen)
         message(screen, "TRIAL {0}/{1}".format(i + 1, trials))
@@ -114,6 +118,7 @@ def twoback(screen, n):
         message(screen, "TRIAL {0}/{1}".format(i + 1, trials))
         pygame.display.flip()
         time.sleep(2.500)
+        vmark, amark = (False, False)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit(0)
@@ -123,14 +128,16 @@ def twoback(screen, n):
                 elif event.unicode == u'l':
                     amark = True
         if i >= n:
-            if vmark and (visual[i] == visual[i - n]):
-                score += 1
-            if vmark and (visual[i] != visual[i - n]):
-                score -= 1
-            if amark and (audio[i] == audio[i - n]):
-                score += 1
-            if amark and (audio[i] != audio[i - n]):
-                score -= 1
+            if vmark:
+                if visual[i] == visual[i - n]:
+                    score += 1
+                else:
+                    score -= 1
+            if amark:
+                if audio[i] == audio[i - n]:
+                    score += 1
+                else:
+                    score -= 1
     screen.fill((0, 0, 0))
     drawaxes(screen)
     message(screen, "SCORE: {0}/12".format(score))
@@ -140,8 +147,13 @@ def twoback(screen, n):
     return score
 
 if __name__ == '__main__':
+    if not os.path.exists('snd'):    
+        os.mknod('snd', stat.S_IFIFO | 0600)
     pygame.init()
     fontname = pygame.font.get_default_font()
     font = pygame.font.Font(fontname, 24)
     screen = pygame.display.set_mode((300, 350), 0, 24)
-    oneback(screen, 3)
+    # Dual n-back with n = 1
+    twoback(screen, n=1)
+    ## Single n-back with n = 2
+    #oneback(screen, n=2)
