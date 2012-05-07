690172799906f71b12481fde820cf1b8255d9060
Christopher Erwin <chrisferwin@gmail.com>
2012-05-07 16:54:59 -0700

    Rewrote nback functions to use a single function

--- nback.py
+++ nback.py
@@ -31,7 +31,7 @@ visual_points = {(0, 0), (0, 1), (0, 2), (2, 0), (2, 1), (2, 2), (1, 0), (1, 2)}
 audio_points = set("AVSOMUIT")
 
 def gentrials(n, points):
-    trials = 20 + n
+    trials = 2 + n
     tlist = []
     target = [True] * 6 + [False] * 14
     random.shuffle(target)
@@ -46,102 +46,83 @@ def gentrials(n, points):
         tlist.append(p)
     return tlist
 
-def oneback(screen, n):
-    """Single n-back"""
+def nback(screen, n, type):
+    """An n-back session. Type determines single or double n-back"""
     trials = 20 + n
-    visual = gentrials(n, visual_points)
     score = 0
+    visual = gentrials(n, visual_points)
+    if (type > 1):
+      audio = gentrials(n, audio_points)
 
+    # Set up the screen
     drawaxes(screen)
     message(screen, "STARTING")
     pygame.display.flip()
     time.sleep(2.0)
     pygame.event.clear()
+
+    # Draw the trials
     for i in range(trials):
         screen.fill((0, 0, 0))
         drawaxes(screen)
         message(screen, "TRIAL {0}/{1}".format(i + 1, trials))
         (x, y) = visual[i]
         drawsquare(screen, x, y)
+        if (type > 1):
+            os.system("espeak -w snd {0} & paplay snd &".format(audio[i]))
         pygame.display.flip()
-        time.sleep(0.500)
+
+        if (type > 1):
+            time.sleep(2.500)
+        else:
+            time.sleep(0.500)
 
         screen.fill((0, 0, 0))
         drawaxes(screen)
         message(screen, "TRIAL {0}/{1}".format(i + 1, trials))
         pygame.display.flip()
-        time.sleep(2.500)
+        if (type == 1):
+            time.sleep(2.500)
         vmark = False
+        if (type > 1):
+            amark = False
+
+        # Handle keyboard input
         for event in pygame.event.get():
+            # Quit
             if event.type == pygame.QUIT:
                 sys.exit(0)
             elif event.type == pygame.KEYDOWN:
+                # Visual
                 if event.unicode == u'a':
                     vmark = True
-        if i >= n:
-            if vmark and (visual[i] == visual[i - n]):
-                score += 1
-            if vmark and (visual[i] != visual[i - n]):
-                score -= 1
-    screen.fill((0, 0, 0))
-    drawaxes(screen)
-    message(screen, "SCORE: {0}/6".format(score))
-    print "SCORE: {0}/6".format(score)
-    pygame.display.flip()
-    time.sleep(0.5)
-    return score
-
-def twoback(screen, n):
-    """Dual n-back"""
-    trials = 20 + n
-    visual = gentrials(n, visual_points)
-    audio = gentrials(n, audio_points)
-    score = 0
-
-    drawaxes(screen)
-    message(screen, "STARTING")
-    pygame.display.flip()
-    time.sleep(2.0)
-    pygame.event.clear()
-    for i in range(trials):
-        screen.fill((0, 0, 0))
-        drawaxes(screen)
-        message(screen, "TRIAL {0}/{1}".format(i + 1, trials))
-        (x, y) = visual[i]
-        drawsquare(screen, x, y)
-        os.system("espeak -w snd {0} & paplay snd &".format(audio[i]))
-        pygame.display.flip()
-        time.sleep(0.500)
-
-        screen.fill((0, 0, 0))
-        drawaxes(screen)
-        message(screen, "TRIAL {0}/{1}".format(i + 1, trials))
-        pygame.display.flip()
-        time.sleep(2.500)
-        vmark, amark = (False, False)
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit(0)
-            elif event.type == pygame.KEYDOWN:
-                if event.unicode == u'a':
-                    vmark = True
-                elif event.unicode == u'l':
+                # Audio
+                if event.unicode == u'l' and type > 1:
                     amark = True
         if i >= n:
             if vmark:
-                if visual[i] == visual[i - n]:
+                if (visual[i] == visual[i - n]):
                     score += 1
-                else:
+
+                elif (visual[i] != visual[i - n]):
                     score -= 1
+
             if amark:
                 if audio[i] == audio[i - n]:
                     score += 1
                 else:
                     score -= 1
+
     screen.fill((0, 0, 0))
     drawaxes(screen)
-    message(screen, "SCORE: {0}/12".format(score))
-    print "SCORE: {0}/12".format(score)
+
+    if (type == 1):
+        message(screen, "SCORE: {0}/6".format(score))
+        print "SCORE: {0}/6".format(score)
+    else:
+        message(screen, "SCORE: {0}/12".format(score))
+        print "SCORE: {0}/12".format(score)
+
     pygame.display.flip()
     time.sleep(0.5)
     return score
@@ -153,7 +134,9 @@ if __name__ == '__main__':
     fontname = pygame.font.get_default_font()
     font = pygame.font.Font(fontname, 24)
     screen = pygame.display.set_mode((300, 350), 0, 24)
+
     # Dual n-back with n = 1
-    twoback(screen, n=1)
+    nback(screen, n=1, type=2)
+
     ## Single n-back with n = 2
-    #oneback(screen, n=2)
+    #nback(screen, n=2, type=1)
